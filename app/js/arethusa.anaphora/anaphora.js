a253a12d4042d86a6bbfb3f3b7c09a900f330e70
LFDM <1986gh@gmail.com>
2014-12-03 15:04:25 +0100

    Add anaphora clickAction

--- app/js/arethusa.anaphora/anaphora.js
+++ app/js/arethusa.anaphora/anaphora.js
@@ -3,7 +3,8 @@
 angular.module('arethusa.anaphora').service('anaphora', [
   'state',
   'configurator',
-  function(state, configurator) {
+  'globalSettings',
+  function(state, configurator, globalSettings) {
     var self = this;
     this.name = 'anaphora';
 
@@ -15,8 +16,32 @@ angular.module('arethusa.anaphora').service('anaphora', [
       configurator.getConfAndDelegate(self);
     }
 
+    function Anaphora() {
+      this.targetId = undefined;
+    }
+
+    function addContainer() {
+      angular.forEach(state.tokens, function(token, i) {
+        if (!token.anaphora) token.anaphora = new Anaphora();
+      });
+    }
+
+    var clickActionName = 'co-reference';
+
+    function clickAction(targetId) {
+      state.doBatched(function() {
+        angular.forEach(state.clickedTokens, function(type, id) {
+          var token = state.getToken(id);
+          state.change(token, 'anaphora.targetId', targetId);
+        });
+      });
+    }
+
+    globalSettings.addClickAction(clickActionName, clickAction);
+
     this.init = function() {
       configure();
+      addContainer();
     };
   }
 ]);
