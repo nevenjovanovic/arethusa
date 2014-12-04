d14e26e438f0ca64ba17dde2cee722eda2a5ae46
LFDM <1986gh@gmail.com>
2014-12-04 17:52:37 +0100

    Fix clickAction tracking in directive

--- app/js/arethusa.core/directives/global_click_action.js
+++ app/js/arethusa.core/directives/global_click_action.js
@@ -9,7 +9,6 @@ angular.module('arethusa.core').directive('globalClickAction', [
       link: function(scope, element, attrs) {
         scope.gS = globalSettings;
         scope.label = globalSettings.settings.clickAction.label;
-        scope.currentAction = globalSettings.clickAction;
       },
       templateUrl: 'templates/arethusa.core/global_click_action.html'
     };
