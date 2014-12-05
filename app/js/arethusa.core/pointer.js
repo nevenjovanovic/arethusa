c35697acf0de570754261c17c05b9e0b5732d0fb
LFDM <1986gh@gmail.com>
2014-12-05 17:12:27 +0100

    Minor fix in pointer

--- app/js/arethusa.core/pointer.js
+++ app/js/arethusa.core/pointer.js
@@ -15,6 +15,10 @@ angular.module('arethusa.core').service('pointer', [
       self.active = true;
       state.deselectAll();
       tokens().addClass(modeClass);
+      // In case we're already in that mode we need to deregister
+      // the old listener, so that we don't run two actions at the
+      // same time.
+      if (listener) listener();
       listener = state.on('tokenClicked', function(ev, id) {
         onSelection(id, state.getToken(id));
         state.deselectAll();
